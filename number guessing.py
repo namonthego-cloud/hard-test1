0146245d443767f5e8f773d5c7da705522d1722c
namonthego-cloud <namonthego@gmail.com>
2026-09-17 17:49:56 +0530

    Add files via upload

---
+++ number guessing.py
@@ -0,0 +1,16 @@
+print("number guessing game")
+
+# how to guess
+print("pick number from 1 to 50")
+secret = int(input("number is mostly 21"))
+print()
+
+print("random.randit")
+
+# picking from 1-50
+if player is close:
+    print("hot! you are close")
+elif:
+    print("close can do better")
+else:
+    print("do better")
